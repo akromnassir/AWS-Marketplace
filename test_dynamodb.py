95bd78895c70a78734f4efb2d3700bd2611c2e8d
Akrom Nasyrov <85175356+akromnassir@users.noreply.github.com>
2025-02-16 15:22:01 +0500

    Add files via upload

--- test_dynamodb.py
+++ test_dynamodb.py
@@ -10,64 +10,139 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-import json
-
-from botocore.awsrequest import AWSResponse
-
-from boto3.dynamodb.conditions import Attr
+from boto3.docs.service import ServiceDocumenter
 from boto3.session import Session
-from tests import mock, unittest
+from tests.functional.docs import BaseDocsFunctionalTests
 
 
-class TestDynamoDB(unittest.TestCase):
+class TestDynamoDBCustomizations(BaseDocsFunctionalTests):
     def setUp(self):
-        self.http_response = AWSResponse(None, 200, {}, None)
-        self.parsed_response = {}
-        self.make_request_patch = mock.patch(
-            'botocore.endpoint.Endpoint.make_request'
+        super().setUp()
+        self.documenter = ServiceDocumenter(
+            'dynamodb',
+            session=Session(region_name='us-east-1'),
+            root_docs_path=self.root_services_path,
         )
-        self.make_request_mock = self.make_request_patch.start()
-        self.make_request_mock.return_value = (
-            self.http_response,
-            self.parsed_response,
+        self.generated_contents = self.documenter.document_service()
+        self.generated_contents = self.generated_contents.decode('utf-8')
+
+    def test_batch_writer_is_documented(self):
+        self.assert_contains_lines_in_order(
+            [
+                '=========',
+                'Resources',
+                '=========',
+                'The available resources are:',
+                '  dynamodb/table/index',
+            ],
+            self.generated_contents,
         )
-        self.session = Session(
-            aws_access_key_id='dummy',
-            aws_secret_access_key='dummy',
-            region_name='us-east-1',
+        self.assert_contains_lines_in_order(
+            [
+                '.. py:class:: DynamoDB.Table(name)',
+                '  batch_writer',
+            ],
+            self.get_nested_file_contents('dynamodb', 'table', 'index'),
+        )
+        self.assert_contains_lines_in_order(
+            [
+                '************\nbatch_writer\n************',
+                '.. py:method:: DynamoDB.Table.batch_writer(overwrite_by_pkeys=None)',
+            ],
+            self.get_nested_file_contents('dynamodb', 'table', 'batch_writer'),
         )
 
-    def tearDown(self):
-        self.make_request_patch.stop()
-
-    def test_resource(self):
-        dynamodb = self.session.resource('dynamodb')
-        table = dynamodb.Table('MyTable')
-        # Make sure it uses the high level interface
-        table.scan(FilterExpression=Attr('mykey').eq('myvalue'))
-        request = self.make_request_mock.call_args_list[0][0][1]
-        request_params = json.loads(request['body'].decode('utf-8'))
-        assert request_params == {
-            'TableName': 'MyTable',
-            'FilterExpression': '#n0 = :v0',
-            'ExpressionAttributeNames': {'#n0': 'mykey'},
-            'ExpressionAttributeValues': {':v0': {'S': 'myvalue'}},
-        }
-
-    def test_client(self):
-        dynamodb = self.session.client('dynamodb')
-        # Make sure the client still uses the botocore level interface
-        dynamodb.scan(
-            TableName='MyTable',
-            FilterExpression='#n0 = :v0',
-            ExpressionAttributeNames={'#n0': 'mykey'},
-            ExpressionAttributeValues={':v0': {'S': 'myvalue'}},
+    def test_document_interface_is_documented(self):
+        put_item_content = self.get_nested_file_contents(
+            'dynamodb', 'table', 'put_item'
         )
-        request = self.make_request_mock.call_args_list[0][0][1]
-        request_params = json.loads(request['body'].decode('utf-8'))
-        assert request_params == {
-            'TableName': 'MyTable',
-            'FilterExpression': '#n0 = :v0',
-            'ExpressionAttributeNames': {'#n0': 'mykey'},
-            'ExpressionAttributeValues': {':v0': {'S': 'myvalue'}},
-        }
+        self.assert_contains_lines_in_order(
+            [
+                '********',
+                'put_item',
+                '********',
+                '.. py:method:: DynamoDB.Table.put_item(**kwargs)',
+                # Make sure the request syntax is as expected.
+                'response = table.put_item(',
+                'Item={',
+                (
+                    '\'string\': \'string\'|123|Binary(b\'bytes\')'
+                    '|True|None|set([\'string\'])|set([123])|'
+                    'set([Binary(b\'bytes\')])|[]|{}'
+                ),
+                '},',
+                'Expected={',
+                '\'string\': {',
+                (
+                    '\'Value\': \'string\'|123'
+                    '|Binary(b\'bytes\')|True|None|set([\'string\'])'
+                    '|set([123])|set([Binary(b\'bytes\')])|[]|{},'
+                ),
+                '\'AttributeValueList\': [',
+                (
+                    '\'string\'|123|Binary(b\'bytes\')'
+                    '|True|None|set([\'string\'])|set([123])|'
+                    'set([Binary(b\'bytes\')])|[]|{},'
+                ),
+                # Make sure the request parameter is documented correctly.
+                ':type Item: dict',
+                ':param Item: **[REQUIRED]**',
+                '- *(string) --*',
+                (
+                    '- *(valid DynamoDB type) -- *- The value of the '
+                    'attribute. The valid value types are listed in the '
+                    ':ref:`DynamoDB Reference Guide<ref_valid_dynamodb_types>`.'
+                ),
+                # Make sure the response syntax is as expected.
+                '{',
+                '\'Attributes\': {',
+                (
+                    '\'string\': \'string\'|123|'
+                    'Binary(b\'bytes\')|True|None|set([\'string\'])|'
+                    'set([123])|set([Binary(b\'bytes\')])|[]|{}'
+                ),
+                '},',
+                # Make sure the response parameter is documented correctly.
+                '- **Attributes** *(dict) --*',
+                '- *(string) --*',
+                (
+                    '- *(valid DynamoDB type) -- *- The value of '
+                    'the attribute. The valid value types are listed in the '
+                    ':ref:`DynamoDB Reference Guide<ref_valid_dynamodb_types>`.'
+                ),
+            ],
+            put_item_content,
+        )
+
+    def test_conditions_is_documented(self):
+        query_contents = self.get_nested_file_contents(
+            'dynamodb', 'table', 'query'
+        )
+        self.assert_contains_lines_in_order(
+            [
+                # Make sure the request syntax is as expected.
+                'response = table.query(',
+                ('FilterExpression=Attr(\'myattribute\').' 'eq(\'myvalue\'),'),
+                ('KeyConditionExpression=Key(\'mykey\')' '.eq(\'myvalue\'),'),
+                # Make sure the request parameter is documented correctly.
+                (
+                    ':type FilterExpression: condition from '
+                    ':py:class:`boto3.dynamodb.conditions.Attr` method'
+                ),
+                (
+                    ':param FilterExpression: The condition(s) an '
+                    'attribute(s) must meet. Valid conditions are listed in '
+                    'the :ref:`DynamoDB Reference Guide<ref_dynamodb_conditions>`.'
+                ),
+                (
+                    ':type KeyConditionExpression: condition from '
+                    ':py:class:`boto3.dynamodb.conditions.Key` method'
+                ),
+                (
+                    ':param KeyConditionExpression: The condition(s) a '
+                    'key(s) must meet. Valid conditions are listed in the '
+                    ':ref:`DynamoDB Reference Guide<ref_dynamodb_conditions>`.'
+                ),
+            ],
+            query_contents,
+        )
--- test_smoke.py
+++ test_smoke.py
@@ -10,78 +10,211 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+import os
+import shutil
+import tempfile
+
 import botocore.session
 import pytest
+from botocore import xform_name
+from botocore.exceptions import DataNotFoundError
 
-from boto3.session import Session
+import boto3
+from boto3.docs.service import ServiceDocumenter
 
-boto3_session = None
+DOCS_ROOT_DIR = None
+ROOT_SERVICES_PATH = None
 
 
-def create_session():
-    global boto3_session
-    if boto3_session is None:
-        boto3_session = Session(
-            aws_access_key_id='dummy',
-            aws_secret_access_key='dummy',
-            region_name='us-east-1',
+@pytest.fixture
+def botocore_session():
+    return botocore.session.get_session()
+
+
+@pytest.fixture
+def boto3_session():
+    return boto3.Session(region_name='us-east-1')
+
+
+def all_services():
+    # Create temporary directory before testing.
+    global DOCS_ROOT_DIR, ROOT_SERVICES_PATH
+    DOCS_ROOT_DIR = tempfile.mkdtemp()
+    ROOT_SERVICES_PATH = os.path.join(
+        tempfile.mkdtemp(), 'reference', 'services'
+    )
+    session = boto3.Session(region_name='us-east-1')
+    yield from session.get_available_services()
+    # Clean up temporary directory after testing.
+    shutil.rmtree(DOCS_ROOT_DIR)
+
+
+@pytest.fixture
+def available_resources():
+    session = boto3.Session(region_name='us-east-1')
+    return session.get_available_resources()
+
+
+@pytest.mark.parametrize('service_name', all_services())
+def test_documentation(
+    boto3_session, botocore_session, available_resources, service_name
+):
+    generated_docs = ServiceDocumenter(
+        service_name, session=boto3_session, root_docs_path=ROOT_SERVICES_PATH
+    ).document_service()
+    generated_docs = generated_docs.decode('utf-8')
+    client = boto3.client(service_name, 'us-east-1')
+
+    # Check that all of the services have the appropriate title
+    _assert_has_title(generated_docs, client)
+
+    # Check that all services have the client documented.
+    _assert_has_client_documentation(generated_docs, service_name, client)
+
+    # If the service has resources, make sure the service resource
+    # is at least documented.
+    if service_name in available_resources:
+        resource = boto3.resource(service_name, 'us-east-1')
+        _assert_has_resource_documentation(
+            generated_docs, service_name, resource
         )
 
-    return boto3_session
+    # If the client can paginate, make sure the paginators are documented.
+    try:
+        paginator_model = botocore_session.get_paginator_model(service_name)
+        if paginator_model._paginator_config:
+            _assert_has_paginator_documentation(
+                generated_docs,
+                service_name,
+                client,
+                sorted(paginator_model._paginator_config),
+            )
+    except DataNotFoundError:
+        pass
+
+    # If the client has waiters, make sure the waiters are documented.
+    if client.waiter_names:
+        waiter_model = botocore_session.get_waiter_model(service_name)
+        _assert_has_waiter_documentation(
+            generated_docs, service_name, client, waiter_model
+        )
 
 
-def _all_resources():
-    session = create_session()
-    for service_name in session.get_available_resources():
-        yield session, service_name
+def _assert_contains_lines_in_order(lines, contents):
+    for line in lines:
+        assert line in contents
+        beginning = contents.find(line)
+        contents = contents[(beginning + len(line)) :]
 
 
-def _all_clients():
-    session = create_session()
-    for service_name in session.get_available_services():
-        yield session, service_name
+def _assert_has_title(generated_docs, client):
+    title = client.__class__.__name__
+    ref_lines = ['*' * len(title), title, '*' * len(title)]
+    _assert_contains_lines_in_order(ref_lines, generated_docs)
 
 
-def _all_api_version_args():
-    botocore_session = botocore.session.get_session()
-    boto3_session = create_session()
-    for service_name in boto3_session.get_available_resources():
-        yield (service_name, botocore_session, boto3_session)
+def _assert_has_client_documentation(generated_docs, service_name, client):
+    class_name = client.__class__.__name__
+    ref_lines = [
+        '======',
+        'Client',
+        '======',
+        f'.. py:class:: {class_name}.Client',
+        '  A low-level client representing',
+        '    import boto3',
+        f'    client = boto3.client(\'{service_name}\')',
+        'These are the available methods:',
+        f'  {service_name}/client/get_paginator',
+        f'  {service_name}/client/get_waiter',
+    ]
+    _assert_contains_lines_in_order(ref_lines, generated_docs)
+    for method_name in ['get_paginator', 'get_waiter']:
+        _assert_contains_lines_in_order(
+            [
+                f'{method_name}',
+                f'.. py:method:: {client.__class__.__name__}.Client.{method_name}(',
+            ],
+            get_nested_file_contents(service_name, 'client', method_name),
+        )
 
 
-@pytest.mark.parametrize('resource_args', _all_resources())
-def test_can_create_all_resources(resource_args):
-    """Verify we can create all existing resources."""
-    session, service_name = resource_args
-    resource = session.resource(service_name)
-    # Verifying we have a "meta" attr is just an arbitrary
-    # sanity check.
-    assert hasattr(resource, 'meta')
+def _assert_has_paginator_documentation(
+    generated_docs, service_name, client, paginator_names
+):
+    ref_lines = [
+        '==========',
+        'Paginators',
+        '==========',
+        'The available paginators are:',
+    ]
+    for paginator_name in paginator_names:
+        ref_lines.append(f'  {service_name}/paginator/{paginator_name}')
+
+    for paginator_name in paginator_names:
+        _assert_contains_lines_in_order(
+            [
+                f'.. py:class:: {client.__class__.__name__}.Paginator.{paginator_name}',
+                '  .. py:method:: paginate(',
+            ],
+            get_nested_file_contents(
+                service_name, 'paginator', paginator_name
+            ),
+        )
+
+    _assert_contains_lines_in_order(ref_lines, generated_docs)
 
 
-@pytest.mark.parametrize('client_args', _all_clients())
-def test_can_create_all_clients(client_args):
-    """Verify we can create all existing clients."""
-    session, service_name = client_args
-    client = session.client(service_name)
-    assert hasattr(client, 'meta')
+def _assert_has_waiter_documentation(
+    generated_docs, service_name, client, waiter_model
+):
+    ref_lines = ['=======', 'Waiters', '=======', 'The available waiters are:']
+    for waiter_name in waiter_model.waiter_names:
+        ref_lines.append(f'  {service_name}/waiter/{waiter_name}')
+
+    for waiter_name in waiter_model.waiter_names:
+        _assert_contains_lines_in_order(
+            [
+                f'.. py:class:: {client.__class__.__name__}.Waiter.{waiter_name}',
+                f'    waiter = client.get_waiter(\'{xform_name(waiter_name)}\')',
+                '  .. py:method:: wait(',
+            ],
+            get_nested_file_contents(service_name, 'waiter', waiter_name),
+        )
+
+    _assert_contains_lines_in_order(ref_lines, generated_docs)
 
 
-@pytest.mark.parametrize('api_version_args', _all_api_version_args())
-def test_api_versions_synced_with_botocore(api_version_args):
-    """Verify both boto3 and botocore clients stay in sync."""
-    service_name, botocore_session, boto3_session = api_version_args
-    resource = boto3_session.resource(service_name)
-    boto3_api_version = resource.meta.client.meta.service_model.api_version
-    client = botocore_session.create_client(
+def _assert_has_resource_documentation(generated_docs, service_name, resource):
+    ref_lines = [
+        '=======',
+        'Resources',
+        '=======',
+        'The available resources are:',
+    ]
+    ref_lines.append(f'  {service_name}/service-resource/index')
+    _assert_contains_lines_in_order(ref_lines, generated_docs)
+
+    service_resource_ref_lines = [
+        '================',
+        'Service Resource',
+        '================',
+        f'.. py:class:: {resource.meta.client.__class__.__name__}.ServiceResource',
+        '  A resource representing',
+        '    import boto3',
+        f'    {service_name} = boto3.resource(\'{service_name}\')',
+    ]
+    _assert_contains_lines_in_order(
+        service_resource_ref_lines,
+        get_nested_file_contents(service_name, 'service-resource', 'index'),
+    )
+
+
+def get_nested_file_contents(service_name, sub_folder, file_name):
+    service_file_path = os.path.join(
+        ROOT_SERVICES_PATH,
         service_name,
-        region_name='us-east-1',
-        aws_access_key_id='foo',
-        aws_secret_access_key='bar',
+        sub_folder,
+        f'{file_name}.rst',
     )
-    botocore_api_version = client.meta.service_model.api_version
-    err = (
-        f"Different latest API versions found for {service_name}: "
-        f"{botocore_api_version} (botocore), {boto3_api_version} (boto3)\n"
-    )
-    assert botocore_api_version == boto3_api_version, err
+    with open(service_file_path, 'rb') as f:
+        return f.read().decode('utf-8')
